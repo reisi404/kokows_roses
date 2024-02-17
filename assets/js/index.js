21c8a57208f60fbf444aaacd6138ffd303eaf394
Reis Lutolli <rlutolli@york.citycollege.eu>
2024-02-17 19:48:30 +0200

    fixed highlight

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,19 @@
 // spoiler alert, i am totally new into javaaaa-script, so i will definitely have tons of unaware issues here
 
 // Check screen width and height every 1 second
-setInterval(function() {
+setInterval(function () {
     if (window.innerWidth > 700) {
         alert("Aren't you brilliant one? Unfortunately though, I haven't designed it to open this vertically, so you'll have to rotate your phone horizontally and then click the button.");
         location.reload();
     }
 }, 3000);
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
     const texts = document.querySelectorAll('.highlight');
 
-    texts.forEach(text => {
+    texts.forEach((text, index) => {
+        text.style.animationDelay = `${index * 1.0}s`;
         text.addEventListener('animationend', () => {
             text.classList.remove('highlighted');
         });
@@ -30,7 +32,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
         setTimeout(() => {
             text.classList.add('highlighted');
-        }, 7000);
+        }, 5500);
     });
 });
-
